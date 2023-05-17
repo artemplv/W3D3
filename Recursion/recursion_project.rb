afa1b1cef8a999ef77a26534d46637efa987c9dd
artemplv <artemplv@gmail.com>
2023-05-17 12:06:45 -0400

    complete exp methods

--- Recursion/recursion_project.rb
+++ Recursion/recursion_project.rb
@@ -15,3 +15,26 @@ def range_it(start_pos,end_pos)
 end
 
 # p range_it(3,7)
+
+def exp_1(base, power)
+    return 1 if power == 0
+
+    base * exp_1(base, power - 1)
+end
+
+# p exp_1(2, 8)
+
+def exp_2(base, power)
+    retrun 1 if power == 0
+    return base if power == 1
+
+    if power.even?
+        temp = exp_2(base, power / 2)
+        temp * temp
+    else
+        temp = exp_2(base, (power - 1) / 2)
+        base * temp * temp
+    end
+end
+
+p exp_2(2, 7)
