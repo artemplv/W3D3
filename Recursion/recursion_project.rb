434c5e3a9c79478aaf06293958deeb521bbae1d1
Joe Randazzo <jprandazzo@icloud.com>
2023-05-17 14:15:44 -0400

    deep dup

--- Recursion/recursion_project.rb
+++ Recursion/recursion_project.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 def range_rec(start_pos,end_pos)
     return [] if end_pos <= start_pos
     range_rec(start_pos,end_pos-1) << end_pos-1
@@ -37,4 +39,40 @@ def exp_2(base, power)
     end
 end
 
-p exp_2(2, 7)
+# p exp_2(2, 7)
+
+#-----------
+#Deep dup
+
+class Array
+
+    def deep_dup
+        return self if self.class != Array
+        arr = []
+
+        self.each do |ele|
+            sub_arr = []
+            if ele.class == Array
+                sub_arr += ele.deep_dup
+            else
+                arr << ele
+            end
+            arr << sub_arr unless sub_arr.empty?
+        end
+        
+        arr
+        
+    end
+end
+
+#[4]
+
+# [4, [1,2,3],[4,5,6]]
+# p [4].deep_dup
+arr = [1, [2], [3, [4]]]
+arr_2 = arr.deep_dup
+arr[2] = "x"
+p arr
+p arr_2
+# p arr
+# p [[1,2,3],[4,5,6]].deep_dup
